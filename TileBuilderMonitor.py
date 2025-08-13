f670565fb0877b6f9e8fc0d6c516dd3fa5d07be3
Raghav Sharma <ragsharm@atlvscode0002.amd.com>
2025-08-13 16:05:19 -0400

    pull the username from printenv

--- TileBuilderMonitor.py
+++ TileBuilderMonitor.py
@@ -1,2 +1,17 @@
-print("hello world")
-
+#!/usr/bin/env python3.10
+ 
+import subprocess
+ 
+def get_user_with_printenv():
+    try:
+        result = subprocess.run(["printenv", "USER"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+ 
+        user = result.stdout.strip()
+        return user
+    except subprocess.CalledProcessError as e:
+        print("printenv command failed:", e.stderr.strip())
+        return None
+ 
+if __name__ == "__main__":
+    user_from_cmd = get_user_with_printenv()
+    print("USER from printenv:", user_from_cmd)
